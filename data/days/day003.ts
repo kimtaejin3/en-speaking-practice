2f38a264e00161522faeaf6b2b0d3753d40f643a
kimtaejin3 <rlaxowls1316@naver.com>
2026-02-24 21:15:38 +0900

    feat add new posts

--- data/days/day003.ts
+++ data/days/day003.ts
@@ -2,19 +2,20 @@ import { DayData } from '../types';
 
 const day003: DayData = {
   id: 3,
-  title: '시간 가는 줄 몰랐어',
+  title: `what's the weather like?`,
   conversation: [
-    { speaker: 'A', english: "Wow, it's already 10 PM!", korean: '와, 벌써 밤 10시야!' },
-    { speaker: 'B', english: 'Time flies when you are having fun.', korean: '즐거우면 시간이 빨리 가지.' },
-    { speaker: 'A', english: "That's true. I didn't realize it was this late.", korean: '맞아. 이렇게 늦은 줄 몰랐어.' },
-    { speaker: 'B', english: 'We should probably head home soon.', korean: '슬슬 집에 가야 할 것 같아.' },
-    { speaker: 'A', english: "Yeah, let's call it a night.", korean: '응, 오늘은 여기까지 하자.' },
-    { speaker: 'B', english: "This was really fun. Let's do this again soon.", korean: '정말 재밌었어. 조만간 또 하자.' },
+    { speaker: 'A', english: `what's the weather going to be like this Saturday?`, korean: `이번 주 토요일 날씨가 어떨 것 같아?` },
+    { speaker: 'B', english: `The weather reporter said it is gonna be raining.`, korean: `일기예보에서 비가 올 거라고 했어.` },
+    { speaker: 'A', english: `I get the feeling that we should call off our trip.`, korean: `여행을 취소해야 할 것 같은 느낌이 들어.` },
+    { speaker: 'B', english: `Boy! It totally slipped my mind.`, korean: `이런! 깜빡 잊고 있었네.` },
+    { speaker: 'A', english: `Don't tell me you were going to go alone behind my back.`, korean: `설마 나 몰래 혼자 가려고 했던 건 아니겠지.` },
+    { speaker: 'B', english: `No way. I'll just go with the flow`, korean: `말도 안 돼. 난 그냥 대세를 따를 거야.` },
+  
   ],
   representativeExpression: {
-    english: 'Time flies when you are having fun.',
-    korean: '즐거우면 시간이 빨리 가지.',
-    explanation: '즐거운 시간을 보내다 보면 시간이 빠르게 지나간다는 의미의 표현입니다. "Time flies"는 "시간이 날아가다"라는 뜻으로, 시간이 매우 빠르게 흐른다는 것을 비유적으로 나타냅니다.',
+    english: `what's the weather like?.`,
+    korean: '날씨가 어때?',
+    explanation: `What + be 동산 ~ like? 는 사물이나 사람의 상태를 물을 때 사용하는 표현입니다. '새로 오신 선생님 어때?' 라고 묻는다면 what's your new teacher like? 와 같이 말하면 되겠죠.`,
   },
 };
 
